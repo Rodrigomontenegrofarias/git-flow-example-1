ae2d397ba443847e2b749bedfa8e42378423b39b
nicolasd <nicolas.delgado@alumnos.uv.cl>
2020-11-19 22:59:31 -0300

    asignando version al programa

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {version} from "./../package.json"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Program version : {version}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          For the University of Valparaíso
+        </p>
       </header>
     </div>
   );
